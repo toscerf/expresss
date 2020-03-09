d53c14a6e2e264858d0f5ec9281fc5958d686168
flt <toscerflorent@gmail.com>
2020-03-09 16:20:41 +0100

    add bdd

--- initdb.js
+++ initdb.js
@@ -21,22 +21,26 @@ async function initDb() {
     try {
         client = await MongoClient.connect(url);
         const db = client.db(dbName);
+        console.log('Deleting beers');
         try {
             if (db.collection('beers', { strict: true})) {
                 let dropped = await db.collection('beers').drop();
                 console.log('DB dropped');
             }
         } catch(err) {
-            console.log('Collection not found')
+            console.log('Collection not found', err);
         }
+        console.log('Inserting beers');
         beers.forEach( async (beerName) => {
             let beer = require(`./step-05/beers/${beerName}.json`);
             let inserted = await db.collection('beers').insertOne(beer);
             console.log(`Beer ${beerName} inserted`);
         });
+        return process.exit(0);
     }
     catch(err) {
-        console.log(err);
+        console.log('InitDb error', err);
+        return process.exit(0);
     }
 }
 
